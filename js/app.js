1b717c6d9ea708304a764d15ba004a16d64300f6
FlycNguyen1992 <flycnguyen@gmail.com>
2016-03-02 02:32:42 +0700

    20160302

--- js/app.js
+++ js/app.js
@@ -40,16 +40,32 @@ app.config(['$routeProvider', function ($routeProvider) {
         })
 
 
-        .when('/product', {
+    .when('/product', {
+        templateUrl: 'templates/OurProduct.html',
+        controller: 'productController'
+
+
+    })
+    .when('/search', {
+        templateUrl: 'templates/Search.html',
+        controller: 'searchController'
+
+
+    })
+
+    .when('/product/:typeS', {
             templateUrl: 'templates/OurProduct.html',
-            controller: 'productController'
+            controller: 'typeController'
+
 
         })
+    
+    
         .when('/detail/:id', {
             templateUrl: 'templates/detail.html',
             controller: 'detailController'
         })
-        
+
 		}]);
 
 app.directive('headerDirective', [function () {
@@ -68,6 +84,37 @@ app.directive('footerDirective', [function () {
 app.controller('productController', ['$scope', '$firebaseArray', '$routeParams', function ($scope, $firebaseArray, $routeParams) {
     var ref = new Firebase("https://scorching-inferno-3570.firebaseio.com/");
     $scope.items = $firebaseArray(ref);
+    $scope.currentPage = 0;
+    $scope.pageSize = 12;
+    $scope.numberOfPages = function () {
+        return Math.ceil($scope.items.length / $scope.pageSize);
+    }
+
+
+	    }]);
+
+app.controller('searchController', ['$scope', '$firebaseArray', '$routeParams', function ($scope, $firebaseArray, $routeParams) {
+    var ref = new Firebase("https://scorching-inferno-3570.firebaseio.com/");
+    $scope.items = $firebaseArray(ref);
+    $scope.currentPage = 0;
+    $scope.pageSize = 12;
+    $scope.numberOfPages = function () {
+        return Math.ceil($scope.items.length / $scope.pageSize);
+    }
+
+
+	    }]);
+
+app.controller('typeController', ['$scope', '$firebaseArray', '$routeParams', function ($scope, $firebaseArray, $routeParams) {
+    var ref = new Firebase("https://scorching-inferno-3570.firebaseio.com/");
+    $scope.items = $firebaseArray(ref.orderByChild('typeS').equalTo($routeParams.typeS));
+    $scope.currentPage = 0;
+    $scope.pageSize = 12;
+    $scope.numberOfPages = function () {
+        return Math.ceil($scope.items.length / $scope.pageSize);
+    }
+
+
 	    }]);
 
 app.controller('homeController', ['$scope', '$firebaseArray', function ($scope, $firebaseArray) {
@@ -80,22 +127,44 @@ app.controller('detailController', ['$scope', '$firebaseArray', '$routeParams', 
     $scope.items = $firebaseArray(ref.orderByChild('id').equalTo(id));
 	    }]);
 
-app.controller('mainController',['$scope', '$firebaseArray', function ($scope, $firebaseArray){
-    $scope.cart=[];
-    $scope.numitem=0;
-    $scope.addItem = function(id){
-    
-    if($scope.cart.indexOf(id)<0){
-        $scope.cart.push(id);
-        $scope.numitem += 1;
+app.controller('mainController', ['$window', '$scope', '$firebaseArray', function ($window, $scope, $firebaseArray) {
+    $scope.cart = [];
+    $scope.search = {};
+    $scope.numitem = 0;
+    $scope.addItem = function (id) {
+        var f=true;
+        if ($scope.cart.indexOf(id) < 0) {
+            
+        for (var i = 0;i< $scope.cart.length;i++) {
+            if(id.id == $scope.cart[i].id){
+                f=false;
+            }
+        }
+            if(f){
+            $scope.cart.push(id);
+            $scope.numitem += 1;
+            }
+        };
     };
-    };
-    $scope.remove = function(id){
-        $scope.cart.splice($scope.cart.indexOf(id),1);
+    $scope.remove = function (id) {
+        $scope.cart.splice($scope.cart.indexOf(id), 1);
         $scope.numitem -= 1;
+
+    };
+    
+    $scope.searching=function(sea) {
+        $scope.search.name= sea;
+        $window.location.href = '#search';
         
-    }
+  }
+    
 }]);
 
-
-
+//We already have a limitTo filter built-in to angular,
+//let's make a startFrom filter
+app.filter('startFrom', function () {
+    return function (input, start) {
+        start = +start; //parse to int
+        return input.slice(start);
+    }
+});
