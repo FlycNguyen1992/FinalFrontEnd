f75838c96df75de02d51f56c59456bd634b43008
FlycNguyen1992 <flycnguyen@gmail.com>
2016-06-01 16:09:56 +0700

    Something change

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
-var app = angular.module('store', ['firebase', 'ngRoute','angularUtils.directives.dirPagination','ngAnimate', 'ui.bootstrap']);
+var app = angular.module('store', ['firebase', 'ngRoute', 'angularUtils.directives.dirPagination', 'ngAnimate', 'ui.bootstrap']);
 app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider
         .when('/', {
-            templateUrl: 'templates/Home.html',
-            controller: 'homeController'
+            templateUrl: 'templates/Home.html'
+            , controller: 'homeController'
         })
         .when('/about', {
             templateUrl: 'templates/About.html'
@@ -33,20 +33,20 @@ app.config(['$routeProvider', function ($routeProvider) {
             templateUrl: 'templates/PrivacyPolicy.html'
         })
         .when('/product', {
-            templateUrl: 'templates/OurProduct.html',
-            controller: 'productController'
+            templateUrl: 'templates/OurProduct.html'
+            , controller: 'productController'
         })
         .when('/search', {
-            templateUrl: 'templates/Search.html',
-            controller: 'searchController'
+            templateUrl: 'templates/Search.html'
+            , controller: 'searchController'
         })
         .when('/product/:typeS', {
-        templateUrl: 'templates/OurProduct.html',
-        controller: 'typeController'
+            templateUrl: 'templates/OurProduct.html'
+            , controller: 'typeController'
         })
         .when('/detail/:id', {
-        templateUrl: 'templates/detail.html',
-        controller: 'detailController'
+            templateUrl: 'templates/detail.html'
+            , controller: 'detailController'
         })
 		}]);
 
@@ -69,8 +69,8 @@ app.controller('productController', ['$scope', '$firebaseArray', function ($scop
     $scope.items = $firebaseArray(ref);
     $scope.currentPage = 1;
     $scope.pageSize = 12;
-    
-    
+
+
 	    }]);
 
 app.controller('typeController', ['$scope', '$firebaseArray', '$routeParams', function ($scope, $firebaseArray, $routeParams) {
@@ -92,50 +92,48 @@ app.controller('detailController', ['$scope', '$firebaseArray', '$routeParams', 
     $scope.items = $firebaseArray(ref.orderByChild('id').equalTo(id));
     var listitem = {};
     var listitem2 = [];
-    ref.on('value', function(snapshot){
+    ref.on('value', function (snapshot) {
         listitem = snapshot.val();
-        var arr = $.map(listitem, function(el){
+        var arr = $.map(listitem, function (el) {
             return el;
         })
         var t = 4;
-        for (var i = 0; i < t;i++){
-            var it = arr[Math.floor(Math.random()*arr.length)];
-            if (it.id==id) {
-                if (i!=0) {
-                i--;
-                }
-                else {
+        for (var i = 0; i < t; i++) {
+            var it = arr[Math.floor(Math.random() * arr.length)];
+            if (it.id == id) {
+                if (i != 0) {
+                    i--;
+                } else {
                     t++;
                 }
-                arr.splice(i,1);
-            }
-            else {
+                arr.splice(i, 1);
+            } else {
                 listitem2.push(it);
-                arr.splice(arr.indexOf(it),1);
+                arr.splice(arr.indexOf(it), 1);
             }
         }
         $scope.relateitem = listitem2;
-        
-    }) 
-    
-           
+
+    })
+
+
 	    }]);
 
 app.controller('mainController', ['$window', '$scope', '$firebaseArray', function ($window, $scope, $firebaseArray) {
-   var _selected;
-  $scope.selected = undefined;
-  var ref = new Firebase("https://scorching-inferno-3570.firebaseio.com/");
-  $scope.statesWithFlags = $firebaseArray(ref);
-  $scope.enter = function (id) {
-     $window.location.href = '#detail/'+id.id;  
-  }
- 
+    var _selected;
+    $scope.selected = undefined;
+    var ref = new Firebase("https://scorching-inferno-3570.firebaseio.com/");
+    $scope.statesWithFlags = $firebaseArray(ref);
+    $scope.enter = function (id) {
+        $window.location.href = '#detail/' + id.id;
+    }
 
-    $scope.refer = function() {
+
+    $scope.refer = function () {
         $('.navbar-collapse').toggleClass('in');
     }
-    $scope.scrolltop = function() {
-         window.scrollTo(0, 0);
+    $scope.scrolltop = function () {
+        window.scrollTo(0, 0);
     }
     $scope.cart = [];
     $scope.search = {};
@@ -154,25 +152,28 @@ app.controller('mainController', ['$window', '$scope', '$firebaseArray', functio
             }
         };
     };
-    
+
     $scope.remove = function (id) {
         $scope.cart.splice($scope.cart.indexOf(id), 1);
         $scope.numitem -= 1;
     };
-    
+
     $scope.showConfirm = function () {
-                $window.location.href = '#/';
-                   setTimeout(function () {
-                    $window.location.reload();
-                }, 100);
+        $window.location.href = '#/';
+        $scope.cart = [];
+        $scope.numitem = 0;
+
+        //                   setTimeout(function () {
+        //                    $window.location.reload();
+        //                }, 100);
 
     };
-    
-   
+
+
     $scope.totalprice = function (product) {
         var sum = 0;
         angular.forEach(product, function (t) {
-        sum += (parseInt(t.price) * parseInt(t.quantity));
+            sum += (parseInt(t.price) * parseInt(t.quantity));
         });
         return sum;
     }
